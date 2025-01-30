b83b4109c02f7ca7928b33751831193923977f82
Deontae Wills <willsdeontae06@gmail.com>
2025-01-30 11:04:35 -0600

    Quick update to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer () {
     return (
         <section>
             <footer>
-                <span>&copy;</span> <img className="footerGainaxLogo" src={gainaxLogo} alt="The Gainax Logo" ></img>
+                <span>&copy;</span> <img className="footerGainaxLogo" src={gainaxLogo} alt="The Gainax Logo" ></img> | | Site by <a href="https://deontaewills.netlify.app" target="_blank">Deontae Wills</a>
             </footer>
         </section>
     )
